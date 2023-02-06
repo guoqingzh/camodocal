f7712b237d25c37ae223fd139e92c1f851c4c604
Guoqing Zhang <guoqing.zhang@intel.com>
2023-02-06 15:51:27 +0200

    support multi cameras WIP

--- tools/converter.py
+++ tools/converter.py
@@ -10,27 +10,34 @@ import numpy as np
 
 ##
 # modify the following variables based your case
-# Please note: we only support one camera. 
+# Please note the followings:
+# - The script is coded specifically to support benchmark data 
+#   from https://lifelong-robotic-vision.github.io/dataset/scene.html
+# - we only support one camera. 
 # 
 # For support on this script. please contact 
 #   guoqing.zhang@intel.com
 ##
 
-image_topics = ['/d400/color/image_raw']
-input_rosbag = './cafe1-1.bag'
+
+input_rosbag = os.path.expanduser('~/benchmark_data/lifelong-robotics/cafe1-1.bag')
+basename = os.path.splitext(os.path.basename(input_rosbag))[0]
+output_path = os.path.expanduser('~/converted_benchmark_data/{}'.format(basename))
+data_path = "{}/camodocal_input_data".format(output_path)
+caminfo_path = "{}/camodocal_input_caminfo".format(output_path)
+
+multi_image_topics = ['/d400/color/image_raw','/t265/fisheye1/image_raw' ]
+multi_camera_models = ['PINHOLE', 'MEI']
 odom_topics = ['/odom']
-output_path = "./cali_seq"
-caminfo_topics = ['/d400/color/camera_info']
-caminfo_path = "./caminfo"
-camera_name = "id0" 
+multi_caminfo_topics = ['/d400/color/camera_info', '/t265/fisheye1/camera_info']
 
 odom_connections = []
-image_connections = []
-caminfo_connections = []
+multi_image_connections = []
+multi_caminfo_connections = []
 
 calib_yaml_template = ("%YAML:1.0\n"
         "---\n"
-        "model_type: PINHOLE\n"
+        "model_type: {}\n"
         "camera_name: {}\n"
         "image_width: {}\n"
         "image_height: {}\n"
@@ -59,7 +66,7 @@ def make_folder(path):
         print("Output dir {} is not empty !".format(path))
         exit(0)
 
-make_folder(output_path)
+make_folder(data_path)
 make_folder(caminfo_path)
 
 with Reader(input_rosbag) as reader:
@@ -67,35 +74,41 @@ with Reader(input_rosbag) as reader:
         if connection.topic in odom_topics:
             print("*",connection.topic, connection.msgtype)
             odom_connections.append(connection)
-        elif connection.topic in image_topics:    
-            print("*",connection.topic, connection.msgtype)
-            image_connections.append(connection)    
-        elif connection.topic in caminfo_topics:
-            print("*",connection.topic, connection.msgtype)
-            caminfo_connections.append(connection)
-        else:
-            print(connection.topic, connection.msgtype)
+    
+    for image_topic, caminfo_topic in zip(multi_image_topics, multi_caminfo_topics):
+        for connection in reader.connections:
+            if connection.topic == image_topic:    
+                print("*",connection.topic, connection.msgtype)
+                multi_image_connections.append([connection])    
+            if connection.topic == caminfo_topic:
+                print("*",connection.topic, connection.msgtype)
+                multi_caminfo_connections.append([connection])
+
     if len(odom_connections) == 0:
         print("Error: Missing odom connections!")
         exit(0)
-    if len(image_connections) == 0:
+    if len(multi_image_connections) == 0:
         print("Error: Missing image connections!")
         exit(0)
-    
-    print("Parsing camera info:")
-    for connection, timestamp, rawdata in reader.messages(connections=caminfo_connections):
-        msg = deserialize_ros1(rawdata, connection.msgtype)
-        fn = '{}/camera_{}_calib.yaml'.format(caminfo_path, camera_name)
-        with open(fn, 'w') as writer:
-            writer.write(calib_yaml_template.format(camera_name,msg.width,msg.height, msg.d[0], msg.d[1], msg.d[2], msg.d[3], msg.k[0], msg.k[4], msg.k[2], msg.k[5] ))
-        print("Saved: {}".format(fn))        
+    if len(multi_caminfo_connections) == 0:
+        print("Error: missing camera info connections!")
+        exit(0)
 
+    print("Parsing camera info:")
+    for count, caminfo_connections in enumerate(multi_caminfo_connections):
+        for connection, timestamp, rawdata in reader.messages(connections=caminfo_connections):
+            msg = deserialize_ros1(rawdata, connection.msgtype)
+            fn = '{}/camera_{}_calib.yaml'.format(caminfo_path, count)
+            with open(fn, 'w') as writer:
+                writer.write(calib_yaml_template.format(multi_camera_models[count],"'"+str(count)+"'",msg.width,msg.height, msg.d[0], msg.d[1], msg.d[2], msg.d[3], msg.k[0], msg.k[4], msg.k[2], msg.k[5] ))
+            print("Saved: {}".format(fn))        
+    exit(0)
     print("Parsing odometry data:")
     for connection, timestamp, rawdata in reader.messages(connections=odom_connections):
         msg = deserialize_ros1(rawdata, connection.msgtype)
         r = R.from_quat([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
         r_m = r.as_matrix()
-        fn = '{}/pose_{}.txt'.format(output_path, timestamp)
+        fn = '{}/pose_{}.txt'.format(data_path, timestamp)
         with open(fn, 'w') as writer:
             writer.write('{} {} {} {} {} {} {} {} {} {} {} {}'.format(r_m[0][0], r_m[0][1], r_m[0][2], r_m[1][0], r_m[1][1], r_m[1][2], r_m[2][0], r_m[2][1], r_m[2][2], msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z))
         print("Saved: {}".format(fn))
@@ -113,6 +126,6 @@ with Reader(input_rosbag) as reader:
         image_data_3[:,:,1] = image_data[:,1::3]
         image_data_3[:,:,2] = image_data[:,2::3]
         gray = rgb2gray(image_data_3)
-        fn = '{}/camera_{}_{}.png'.format(output_path, camera_name, timestamp)
+        fn = '{}/camera_{}_{}.png'.format(data_path, camera_name, timestamp)
         plt.imsave( fn, gray, cmap='gray',format='png', vmin=0, vmax=1)
         print("Saved: {}".format(fn))
